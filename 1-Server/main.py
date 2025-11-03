e4b4a522e8c647b709755949d2e46e2e2ce13598
Girelli <sf2943642@gmail.com>
2025-11-03 16:50:38 -0300

    Update main.py

--- 1-Server/main.py
+++ 1-Server/main.py
@@ -71,8 +71,12 @@ async def init_dbadmin():
     global db_pool_admin
     try:
         db_pool_admin = await aiomysql.create_pool(
-            host='localhost', user='root', password='', db='ironwatchadmin',
-            minsize=1, maxsize=10
+            host='localhost', 
+            user='root', 
+            password='', 
+            db='ironwatchadmin',
+            minsize=1, 
+            maxsize=10
         )
         print("[MYSQL ADMIN] Conectado ao Banco de Dados")
     except Exception as e:
@@ -84,8 +88,12 @@ async def init_dbuser():
     global db_pool_user
     try:
         db_pool_user = await aiomysql.create_pool(
-            host='localhost', user='root', password='', db='ironwatch',
-            minsize=1, maxsize=10
+            host='localhost', 
+            user='root', 
+            password='', 
+            db='ironwatch',
+            minsize=1, 
+            maxsize=10
         )
         print("[MYSQL USER] Conectado ao Banco de Dados")
     except Exception as e:
