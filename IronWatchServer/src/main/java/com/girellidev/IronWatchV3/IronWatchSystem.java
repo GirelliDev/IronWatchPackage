c0a67a97ddabff0c93d280c331ea9ab103cee981
GirelliDev <sf2943642@gmail.com>
2026-03-03 14:40:48 -0300

    Adicionando Tela de Login

---
+++ IronWatchServer/src/main/java/com/girellidev/IronWatchV3/IronWatchSystem.java
@@ -0,0 +1,12 @@
+package com.girellidev.ironwatchv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronWatchSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronWatchSystem.class, args);
+    }
+}
