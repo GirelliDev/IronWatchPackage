94f0120e5b67ff8873993d3afe001af7a32d82e7
GirelliDev <sf2943642@gmail.com>
2026-05-11 16:50:30 -0300

    fix #3 fix #4  fix #5

--- IronWatchServer/src/main/java/com/girellidev/ironwatchserver/security/SSLServerSocketProvider.java
+++ IronWatchServer/src/main/java/com/girellidev/ironwatchserver/security/SSLServerSocketProvider.java
@@ -1,53 +1,127 @@
 package com.girellidev.ironwatchserver.security;
 
-import javax.net.ssl.*;
 import java.io.FileInputStream;
 import java.security.KeyStore;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
 public final class SSLServerSocketProvider {
+
+    private static final String KEYSTORE_TYPE = "PKCS12";
+
+    private static final String KEYSTORE_PATH =
+            "security/server.p12";
+
+    private static final String KEYSTORE_PASSWORD =
+            "Colocar_a_merda_da_senha_aqui_gordao";
+
+    private static final String TRUSTSTORE_PATH =
+            "security/truststore.p12";
+
+    private static final String TRUSTSTORE_PASSWORD =
+            "Colocar_a_merda_da_senha_aqui_gordao";
 
     private SSLServerSocketProvider() {
     }
 
-    public static SSLServerSocket criar(
-            int porta,
-            String keystorePath,
-            String keystorePassword
-    ) {
-        try {
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+    public static SSLServerSocket criar(int porta) {
 
-            try (FileInputStream fis = new FileInputStream(keystorePath)) {
-                keyStore.load(fis, keystorePassword.toCharArray());
+        try {
+
+            /*
+             * KEYSTORE
+             */
+
+            KeyStore keyStore =
+                    KeyStore.getInstance(KEYSTORE_TYPE);
+
+            try (FileInputStream fis =
+                         new FileInputStream(KEYSTORE_PATH)) {
+
+                keyStore.load(
+                        fis,
+                        KEYSTORE_PASSWORD.toCharArray()
+                );
             }
 
             KeyManagerFactory keyManagerFactory =
-                    KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                    KeyManagerFactory.getInstance(
+                            KeyManagerFactory.getDefaultAlgorithm()
+                    );
 
-            keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
+            keyManagerFactory.init(
+                    keyStore,
+                    KEYSTORE_PASSWORD.toCharArray()
+            );
 
-            SSLContext sslContext = SSLContext.getInstance("TLS");
+            /*
+             * TRUSTSTORE
+             */
+
+            KeyStore trustStore =
+                    KeyStore.getInstance(KEYSTORE_TYPE);
+
+            try (FileInputStream fis =
+                         new FileInputStream(TRUSTSTORE_PATH)) {
+
+                trustStore.load(
+                        fis,
+                        TRUSTSTORE_PASSWORD.toCharArray()
+                );
+            }
+
+            TrustManagerFactory trustManagerFactory =
+                    TrustManagerFactory.getInstance(
+                            TrustManagerFactory.getDefaultAlgorithm()
+                    );
+
+            trustManagerFactory.init(trustStore);
+
+            /*
+             * SSL CONTEXT
+             */
+
+            SSLContext sslContext =
+                    SSLContext.getInstance("TLS");
 
             sslContext.init(
                     keyManagerFactory.getKeyManagers(),
-                    null,
+                    trustManagerFactory.getTrustManagers(),
                     null
             );
 
-            SSLServerSocketFactory factory = sslContext.getServerSocketFactory();
+            SSLServerSocketFactory factory =
+                    sslContext.getServerSocketFactory();
 
             SSLServerSocket serverSocket =
-                    (SSLServerSocket) factory.createServerSocket(porta);
+                    (SSLServerSocket)
+                            factory.createServerSocket(porta);
 
-            serverSocket.setEnabledProtocols(new String[]{
-                    "TLSv1.2",
-                    "TLSv1.3"
-            });
+            serverSocket.setEnabledProtocols(
+                    new String[]{
+                            "TLSv1.2",
+                            "TLSv1.3"
+                    }
+            );
+
+            /*
+             * Exige certificado do cliente
+             */
+
+            serverSocket.setNeedClientAuth(true);
 
             return serverSocket;
 
         } catch (Exception e) {
-            throw new IllegalStateException("Falha ao criar SSLServerSocket", e);
+
+            throw new IllegalStateException(
+                    "Erro ao iniciar SSL",
+                    e
+            );
         }
     }
 }
