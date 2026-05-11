18884077a6ba69ffd011ecaa768e88683e918a2e
GirelliDev <sf2943642@gmail.com>
2026-05-11 13:58:09 -0300

    fix #262

--- IronWatchServer/src/main/java/com/girellidev/ironwatchserver/security/LoginQrPayload.java
+++ IronWatchServer/src/main/java/com/girellidev/ironwatchserver/security/LoginQrPayload.java
@@ -3,32 +3,19 @@ package com.girellidev.ironwatchserver.security;
 public class LoginQrPayload {
 
     private final String code;
-    private final String login;
-    private final String password;
 
     public LoginQrPayload(String code, String login, String password) {
         this.code = code;
-        this.login = login;
-        this.password = password;
     }
 
     public String getCode() {
         return code;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
     public String toJson() {
         return "{"
                 + "\"code\":\"" + escape(code) + "\","
-                + "\"login\":\"" + escape(login) + "\","
-                + "\"password\":\"" + escape(password) + "\""
                 + "}";
     }
 
